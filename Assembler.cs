17f23bfbec62c78ad7b7edb68594b1ca5bb73300
ManuelCN <manuel.castaneda@upr.edu>
2019-10-03 17:44:18 -0400

    Implemented Pass 1 of Assembler. Creates an instructions list to iterate through in Pass 2 of Assembler.

--- Assembler.cs
+++ Assembler.cs
@@ -10,21 +10,16 @@ namespace PRProcSimulator
     public class Assembler
     {
         private bool containsOrg;
-        private bool matches;
         private long index;
-        private Dictionary<string, string> grammar;
         private List<string> labels;
         private List<string> errors;
+        private List<Instruction> instructions;
         private LinkedList<tableItem> table = new LinkedList<tableItem>();
         public Assembler()
         {
-            //Define Grammar Rules
-            grammar = new Dictionary<string, string>();
-            grammar.Add("[const][ ]+[a-zA-Z0-9]+[ ]+[0-9]+", "CONSTANT"); //Check for CONSTANTS
-
             labels = new List<string>();
             errors = new List<string>();
-            
+            instructions = new List<Instruction>();
         }
 
         public List<string> FilterComments(string input)
@@ -51,48 +46,82 @@ namespace PRProcSimulator
 
             foreach (string line in code)
             {
+                string[] parsedLine;
                 if (Regex.Match(line, "[\torg ][0-9]+").Success && !containsOrg) //Check for ORG
                 {
                     containsOrg = true;
-
-                    index = long.Parse(Regex.Match(line, @"\d+").Value); //find the starting address from origin
-
-                } else if (Regex.Match(line, "[a-zA-Z]+[a-zA-Z0-9]*[:]").Success && containsOrg) //Check for LABELS
+                    index = long.Parse(line.Split(' ')[1]);
+                }
+                else if (Regex.Match(line, "[a-zA-Z]+[a-zA-Z0-9]*[:]").Success && containsOrg) //Check for LABELS
                 {
-                    index += 1;
-                    int endIndex = line.IndexOf(':');
-                    labels.Add(line.Substring(0, endIndex));
-                    //after finding a label puts a new item with param (name,type,address,value)into the table of items
+                    parsedLine = line.Split(' ', ':');
+                    //After finding a label, search in table for the label
+                    //If label is found and does not have an address, update the entry's address
+                    //If label is found and has an address, do nothing
+                    bool error = false;
+                    int tableIndex = searchInRefTable(parsedLine[0]);
+                    if(tableIndex != -1)
+                    {
+                        if (table.ElementAt(tableIndex).address == -1)
+                            table.ElementAt(tableIndex).address = index;
+                        else
+                            error = true;
+                    } else
+                        table.AddLast(new tableItem(parsedLine[0], "label", index, null));
                     
-           
-                    tableItem item = new tableItem(Regex.Match(line, @"[a-zA-Z]+[a-zA-Z0-9]").Value, "label", index,null);
-                } else if (Regex.Match(line, "([a-zA-Z0-9]*[ ]+[d][b][ ]+[)|([0-9]+([ ]*,[ ]*[0-9]+)*)").Success && containsOrg) //Check for LABELSDEF
-                {
-                    index += 1;
-                    labels.Add(line.Split(' ')[0]);
-                } else if (Regex.Match(line, "[const][ ]+[a-zA-Z0-9]+[ ]+[0-9]+").Success && containsOrg) //Check for CONSTANTS
-                {
-                    index += 1;
-                    labels.Add(line.Split(' ')[1]);
-
-                    table.AddLast(new tableItem(Regex.Match(line, @"[ ]+[a-zA-Z0-9]+[ ]+").Value, "const", index, Regex.Match(line, @"[ ]+[0-9]+").Value));
-                   
+                    //If error == true, report error
+                    index++;
                 }
-                else if (Regex.Match(line,"((LOADRIND)|(STORERIND)|(ADD)|(SUV)|(AND)|(OR)|(XOR)|(NOT)|(NEG)|(SHIFTR)|(SHIFTL)|(ROTAR)|(ROTAL)|(JUMPIND)|(GRT)|(GRTEQ)|(EQ)|(NEQ)|(NOP))([ ]*[a-zA-Z0-9]+([ ]*[,][ ]*[a-zA-Z0-9]+)*)").Success && containsOrg)
-                {  //the following instrucctionsare in F1 format 
-                    index += 1;
-                    table.AddLast(new tableItem(Regex.Match(line, @"\t[a-zA-Z]*").Value, "instruction", index, null));
-                   
+                else if (Regex.Match(line, "([a-zA-Z0-9]*[ ]+[d][b][ ]+[)|([0-9]+([ ]*,[ ]*[0-9]+)*)").Success && containsOrg) //Check for LABELSDEF
+                {
+                    parsedLine = line.Split(' ');
+                    table.AddLast(new tableItem(parsedLine[0], "var", index, null));
+                    index++;
                 }
-                else if (Regex.Match(line, "((LOAD)|(LOADIM)|(POP)|(STORE)|(PUSH)|(ADDIM)|(SUBIM)|(LOOP))([ ]*[a-zA-Z0-9]+([ ]*[,][ ]*[a-zA-Z0-9]+)*)").Success && containsOrg)
-                {  //the following instrucctionsare in F2 format 
-                    index += 1;
-                    table.AddLast(new tableItem(Regex.Match(line, @"\t[a-zA-Z]*").Value, "instruction", index, null));
+                else if (Regex.Match(line, "[const][ ]+[a-zA-Z0-9]+[ ]+[0-9]+").Success && containsOrg) //Check for CONSTANTS
+                {
+                    parsedLine = line.Split(' ');
+                    table.AddLast(new tableItem(parsedLine[1], "const", index, parsedLine[2]));
+                    index++;
                 }
                 else if (Regex.Match(line, "((JMPADDR)|(JCONDRIN)|(JCONDADDR))([ ]*[a-zA-Z0-9]+([ ]*[,][ ]*[a-zA-Z0-9]+)*)").Success && containsOrg)
-                {  //the following instrucctionsare in F3 format 
-                    index += 1;
-                    table.AddLast(new tableItem(Regex.Match(line, @"\t[a-zA-Z]*").Value, "instruction", index, null));
+                {
+                    //The following instrucctionsare in F3 format 
+                    if (int.Parse(index.ToString()) % 2 != 0)
+                    {
+                        index++;
+                    }
+                    parsedLine = line.Trim().Split(' ');
+                    instructions.Add(new Instruction(parsedLine[0], getOpcode(parsedLine[0]), parsedLine[1]));
+                    int tableIndex = searchInRefTable(parsedLine[0]);
+                    if (tableIndex == -1)
+                        table.AddLast(new tableItem(parsedLine[1], "label", -1, null));
+                    index++;
+                }
+                else if (Regex.Match(line, "((LOAD)|(LOADIM)|(POP)|(STORE)|(PUSH)|(ADDIM)|(SUBIM)|(LOOP))([ ]*[a-zA-Z0-9]+([ ]*[,][ ]*[a-zA-Z0-9]+)*)").Success && containsOrg)
+                {
+                    //The following instrucctionsare in F2 format 
+                    if (int.Parse(index.ToString()) % 2 != 0)
+                    {
+                        index++;
+                    }
+                    parsedLine = line.Trim().Split(' ', ',');
+                    instructions.Add(new Instruction(parsedLine[0], getOpcode(parsedLine[0]), parsedLine[1], parsedLine[2]));
+                    int tableIndex = searchInRefTable(parsedLine[2]);
+                    if(tableIndex == -1)
+                        table.AddLast(new tableItem(parsedLine[2], "label", -1, null));
+                    index++;
+                }
+                else if (Regex.Match(line,"((LOADRIND)|(STORERIND)|(ADD)|(SUV)|(AND)|(OR)|(XOR)|(NOT)|(NEG)|(SHIFTR)|(SHIFTL)|(ROTAR)|(ROTAL)|(JUMPIND)|(GRT)|(GRTEQ)|(EQ)|(NEQ)|(NOP))([ ]*[a-zA-Z0-9]+([ ]*[,][ ]*[a-zA-Z0-9]+)*)").Success && containsOrg)
+                {
+                    //The following instrucctionsare in F1 format 
+                    if (int.Parse(index.ToString()) % 2 != 0)
+                    {
+                        index++;
+                    }
+                    parsedLine = line.Trim().Split(' ');
+                    instructions.Add(new Instruction(parsedLine[0], getOpcode(parsedLine[0]), parsedLine[1], parsedLine[2], parsedLine[3]));
+                    index++;
                 }
                 else
                 {
@@ -124,7 +153,92 @@ namespace PRProcSimulator
         {
 
         }
+        
+        private string getOpcode(string instr)
+        {
+            switch (instr.ToUpper())
+            {
+                case "LOAD":
+                    return "00000";
+                case "LOADIM":
+                    return "00001";
+                case "POP":
+                    return "00010";
+                case "STORE":
+                    return "00011";
+                case "PUSH":
+                    return "00100";
+                case "LOADRIND":
+                    return "00101";
+                case "STORERIND":
+                    return "00110";
+                case "ADD":
+                    return "00111";
+                case "SUB":
+                    return "01000";
+                case "ADDIM":
+                    return "01001";
+                case "SUBIM":
+                    return "01010";
+                case "AND":
+                    return "01011";
+                case "OR":
+                    return "01100";
+                case "XOR":
+                    return "01101";
+                case "NOT":
+                    return "01110";
+                case "NEG":
+                    return "01111";
+                case "SHIFTR":
+                    return "10000";
+                case "SHIFTL":
+                    return "10001";
+                case "ROTAR":
+                    return "10010";
+                case "ROTAL":
+                    return "10011";
+                case "JMPRIND":
+                    return "10100";
+                case "JMPADDR":
+                    return "10101";
+                case "JCONDRIN":
+                    return "10110";
+                case "JCONDADDR":
+                    return "10111";
+                case "LOOP":
+                    return "11000";
+                case "GRT":
+                    return "11001";
+                case "GRTEQ":
+                    return "11010";
+                case "EQ":
+                    return "11011";
+                case "NEQ":
+                    return "11100";
+                case "NOP":
+                    return "11101";
+                case "CALL":
+                    return "11110";
+                case "RETURN":
+                    return "10101";
+                default:
+                    return "";
+            }
+        }
 
+        private int searchInRefTable(string name)
+        {
+            if(table.Count > 0)
+            {
+                for(int i = 0; i < table.Count; i++)
+                {
+                    if (table.ElementAt(i).name.Equals(name))
+                        return i;
+                }
+            }
+            return -1;
+        }
     }
 }
  //format 1 intructions LOADRIND, STORERIND, ADD, SUV, AND, OR, XOR, NOT, NEG, 
--- Instruction.cs
+++ Instruction.cs
@@ -14,9 +14,56 @@ namespace PRProcSimulator
         public string[] registers;
         public string const_address;
 
-        public Instruction()
+        /// <summary>
+        /// Create Format 1 Instruction
+        /// </summary>
+        /// <param name="kw">Keyword</param>
+        /// <param name="op">Opcode</param>
+        /// <param name="r1">Register A</param>
+        /// <param name="r2">Register B</param>
+        /// <param name="r3">Register C</param>
+        public Instruction(string kw, string op, string r1, string r2, string r3)
         {
-            
+            keyword = kw.ToUpper();
+            format = "F1";
+            opcode = op.ToUpper();
+            registers = new string[3];
+            registers[0] = r1.ToUpper();
+            registers[1] = r2.ToUpper();
+            registers[2] = r3.ToUpper();
+            const_address = null;
+        }
+
+        /// <summary>
+        /// Create Format 2 Instruction
+        /// </summary>
+        /// <param name="kw">Keyword</param>
+        /// <param name="op">Opcode</param>
+        /// <param name="r1">Register A</param>
+        /// <param name="addr">Constant or Address</param>
+        public Instruction(string kw, string op, string r1, string addr)
+        {
+            keyword = kw.ToUpper();
+            format = "F2";
+            opcode = op.ToUpper();
+            registers = new string[1];
+            registers[0] = r1.ToUpper();
+            const_address = addr;
+        }
+
+        /// <summary>
+        /// Create Format 3 Instruction
+        /// </summary>
+        /// <param name="kw">Keyword</param>
+        /// <param name="op">Opcode</param>
+        /// <param name="addr">Constant or Address</param>
+        public Instruction(string kw, string op, string addr)
+        {
+            keyword = kw.ToUpper();
+            format = "F3";
+            opcode = op.ToUpper();
+            registers = null;
+            const_address = addr;
         }
         public void assemble() 
         { 
